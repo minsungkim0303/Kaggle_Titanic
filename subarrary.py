e7b711a19ff47dcaa7cfb39214e0f9b53353c0b6
minsungkim0303 <33391160+minsungkim0303@users.noreply.github.com>
2018-09-29 17:08:42 +0900

    Update subarrary.py
    주석삭제

--- subarrary.py
+++ subarrary.py
@@ -10,7 +10,6 @@ def maxSubArraySum(a,n):
 
 # fuction to check the above fuction
 # a = [4, -2, -1, 1, 5] 
-# a = [ -2, 1, -3, 4, -1, 2, 1, -5, 4]
 #a = [-7, 4, -3, 6, 3, -8, 3, 4]
 a = [1, -2, 3, 4]
-print"Maximum contiguous sum is" , maxSubArraySum(a,len(a)) 
+print"Maximum contiguous sum is" , maxSubArraySum(a,len(a))
